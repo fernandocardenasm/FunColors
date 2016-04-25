c68f126f818de314eee9b9ee5bc2cdac6e2b145c
fernandocardenasm <fernando.cardenasm@gmail.com>
2016-04-25 17:56:46 +0200

    Add Model Application

---
+++ FunColors/Application.swift
@@ -0,0 +1,75 @@
+//
+//  Application.swift
+//  FunColors
+//
+//  Created by Fernando Cardenas on 25/04/16.
+//  Copyright © 2016 Fernando. All rights reserved.
+//
+
+import Foundation
+
+class Application {
+    var score: Int
+    var selectedColor: ColorSelection
+    var circleButtonList: [ButtonCircle]
+    var availableColors: [ColorSelection]
+    var timer: Double
+    var level: Int
+    var numCirclesChanges: Int
+    var numColorsToUse: Int
+    
+    init(){
+        score = 0
+        selectedColor = .Blue
+        circleButtonList = []
+        availableColors = [.Blue, .Red]
+        timer = 3.0
+        level = 1
+        numCirclesChanges = 3
+        numColorsToUse = 2
+    }
+    
+    func allButtonCircles()->[ButtonCircle]{
+        var circleButtons: [ButtonCircle] = []
+        for i in 0...9{
+            circleButtons.append(ButtonCircle(id: i, currentColor: .Blue, lastColor: nil))
+        }
+        return circleButtons
+    }
+    
+    func getRandomColor()->ColorSelection{
+        if useGoldenColor() {
+            return .Gold
+        }
+        else{
+            let randomColorNumber: Int = Int(arc4random_uniform(UInt32(self.availableColors.count - 1)))
+            return self.availableColors[randomColorNumber]
+        }
+    }
+    
+    func useGoldenColor()->Bool{
+        //Max Number: 9, to achieve a 10% chances to get 0, from 0 to 9.
+        let randomNumber:Int = Int(arc4random_uniform(UInt32(9)))
+        if randomNumber == 0 {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    
+}
+
+enum ColorSelection: String {
+    case Red
+    case Blue
+    case Green
+    case Gold
+}
+
+struct ButtonCircle {
+    let id: Int
+    var currentColor:ColorSelection
+    var lastColor: ColorSelection?
+}
+
